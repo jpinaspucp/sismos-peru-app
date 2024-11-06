15484814aa409dca606eee120dd0e5d6e9367efd
jpinaspucp <jpinas@pucp.pe>
2024-11-06 16:28:54 -0500

    mi commit 1 desde mac

---
+++ pages/page1.py
@@ -0,0 +1,20 @@
+# page1.py
+""" import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+def app():
+    st.title("Página 1 - Gráfico de Dispersión")
+    df = px.data.iris()
+    fig = px.scatter(df, x='sepal_width', y='sepal_length', color='species')
+    st.plotly_chart(fig) """
+    
+    # page1.py
+import streamlit as st
+
+def app():
+    st.title("Page 1")
+    st.write("Aquí puedes integrar un cuadro de ArcGIS Dashboard.")
+    #st.components.v1.iframe("URL_DEL_CUADRO_DE_ARCGIS_DASHBOARD", width=700, height=500)
+    st.components.v1.iframe("https://oefa.maps.arcgis.com/apps/dashboards/7430e90bd060492b9a550c01f95d9843", width=700, height=500)
+    
---
+++ pages/page2.py
@@ -0,0 +1,10 @@
+# page2.py
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+def app():
+    st.title("Página 2 - Gráfico de Barras")
+    df = px.data.tips()
+    fig = px.bar(df, x='day', y='total_bill', color='sex', barmode='group')
+    st.plotly_chart(fig)
