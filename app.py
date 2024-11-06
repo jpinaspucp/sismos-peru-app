70b1d7d3f29eeba88eadb46b7244f424c8744263
jpinaspucp <jpinas@pucp.pe>
2024-11-06 16:06:24 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from pages import page1, page2
+
+st.set_page_config(page_title="Mi Aplicación Streamlit", layout="wide")
+
+# Crear un menú de navegación
+menu = ["Página 1", "Página 2"]
+choice = st.sidebar.selectbox("Selecciona una página", menu)
+
+if choice == "Página 1":
+    page1.app()
+elif choice == "Página 2":
+    page2.app()
