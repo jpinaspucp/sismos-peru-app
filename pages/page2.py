537c2f0f032f5ff938b31aedf887327c9010e2ce
jpinaspucp <jpinas@pucp.pe>
2024-11-09 13:46:28 -0500

    commit agol 3

--- pages/page2.py
+++ pages/page2.py
@@ -9,5 +9,5 @@ def app():
     st.title("Page dos")
     st.write("Aquí puedes integrar un cuadro de ArcGIS Dashboard.")
     #st.components.v1.iframe("URL_DEL_CUADRO_DE_ARCGIS_DASHBOARD", width=1700, height=1500)
-    st.components.v1.iframe("https://gismatech4.maps.arcgis.com/apps/dashboards/2c4bf6ca48824b60b1ebcfd10bfdd881", width=1300, height=1000)
+    st.components.v1.iframe("https://gismatech4.maps.arcgis.com/apps/dashboards/2c4bf6ca48824b60b1ebcfd10bfdd881", width=1400, height=600)
     
