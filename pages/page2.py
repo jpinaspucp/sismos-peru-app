6cc1f6e0e3550a27c1c237058ecce331eb0d4411
jpinaspucp <jpinas@pucp.pe>
2024-11-09 13:51:22 -0500

    commit agol 4

--- pages/page2.py
+++ pages/page2.py
@@ -6,8 +6,9 @@ import pandas as pd
 
 
 def app():
-    st.title("Page dos")
-    st.write("Aquí puedes integrar un cuadro de ArcGIS Dashboard.")
+    st.title("Descubre la Seguridad de tu Hogar con Nuestra Herramienta de Visualización Sísmica")
+    st.write("En un mundo donde la seguridad es primordial, conocer la actividad sísmica de tu región puede marcar la diferencia. Nuestra innovadora herramienta de visualización de sismos te permite explorar de manera interactiva los departamentos y sus datos de magnitud de sismos, brindándote información crucial para tomar decisiones informadas.")
+
     #st.components.v1.iframe("URL_DEL_CUADRO_DE_ARCGIS_DASHBOARD", width=1700, height=1500)
-    st.components.v1.iframe("https://gismatech4.maps.arcgis.com/apps/dashboards/2c4bf6ca48824b60b1ebcfd10bfdd881", width=1400, height=600)
+    st.components.v1.iframe("https://gismatech4.maps.arcgis.com/apps/dashboards/2c4bf6ca48824b60b1ebcfd10bfdd881", width=1500, height=900)
     
